88bfc3596b487b3e89b13f09e15ad55f515c5133
Slobodan <sm.slobodanmrvic>
2023-05-27 18:30:36 +0200

    CSS for device section and some changes

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,20 +1,20 @@
 let http = new XMLHttpRequest();
 
-http.open('get', 'phones.json', true);
+http.open("get", "phones.json", true);
 
 http.send();
-http.onload = function (){
-    if(this.readyState == 4 && this.status == 200){
-        let products = JSON.parse(this.responseText);
-        let output = "";
-        for(let item of products){
-            output += `
+http.onload = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let products = JSON.parse(this.responseText);
+    let output = "";
+    for (let item of products) {
+      output += `
             <div id="devicesList">
-          
+                <div class="block">
 				<img src="${item.slika.putanja}" alt="${item.slika.alt}" />
 				<h3>${item.marka} ${item.model}</h3>
 				<ul>
-					<li>Ekran: ${item.specifikacije.ekran}</li>
+					<li>Ekran:  ${item.specifikacije.ekran}</li>
 					<li>RAM: ${item.specifikacije.RAM}</li>
 					<li>Procesor: ${item.specifikacije.procesor}</li>
 					<li>Operativni sistem: ${item.specifikacije.operativniSistem}</li>
@@ -24,12 +24,12 @@ http.onload = function (){
 							<li>Zadnja kamera: ${item.specifikacije.kamera.zadnja}</li>
 						</ul>
 					</li>
-
 				</ul>
-				<h4>${item.cena}</h4>
-				<a class="korpaText" href="#" data-id="${item.id}">DODAJ U KORPU</a>
-      </div> `
-        }
-        document.getElementById("devicesList").innerHTML = output;
+				<h4>${item.cena} <span>RSD</span></h4>
+				<a class="chartText" href="#" data-id="${item.id}">DODAJ U KORPU</a>
+                </div>
+            </div> `;
     }
-}
+    document.getElementById("devicesList").innerHTML = output;
+  }
+};

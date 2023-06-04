3d2befa70c15a746bd52d5a2b62a231d4d835464
Slobodan <sm.slobodanmrvic>
2023-06-04 18:16:07 +0200

    JS for filtering and sorting. login & signup html

--- node-server/server.js
+++ node-server/server.js
@@ -4,26 +4,27 @@ const fs = require("fs");
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.setHeader("Access-Control-Allow_Origin", "*");
+const server = http.createServer((req, res) =>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-type", "application/json");
 
-    if(req.url === "/phones.json") {
-        fs.readFile("phones.json", "utf8", (err, data) => {
+    if(req.url == "/phones.json") {
+        fs.readFile("phones.json", "utf8", (err, data) =>{
             if(err) {
                 res.statusCode = 500;
-                res.end(JSON.stringify({ error: "Internal Server Error" }));
-            } else {
+                res.end(JSON.stringify({error: "Internal Server Error"}));
+            } else{
                 res.statusCode = 200;
                 res.end(data);
             }
         })
-    } else {
+    } else{
         res.statusCode = 404;
-        res.end(JSON.stringify({ error: "Not Found" }))
+        res.end(JSON.stringify({ error: "Not found"}))
     }
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server is running on : http://${hostname}:${port}`);
-})
+    console.log(`Server is running on : http://${hostname}:${port}`)
+});
+

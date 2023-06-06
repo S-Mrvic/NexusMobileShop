55f6ed10de043407c0719fdfabbc6e2668e6798c
Slobodan <sm.slobodanmrvic>
2023-06-06 11:40:00 +0200

    Wrong js file. This is code for signup

--- assets/JS/signup.js
+++ assets/JS/signup.js
@@ -1,43 +1,33 @@
-document.getElementById('login').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    let username = document.getElementById('loginusername').value;
-    let password = document.getElementById('loginpassword').value;
-  
-    // Creating an object with users data
-    let data = {
-      username: username,
-      password: password
-    };
-  
-    // Sending POST request to backhand
-    fetch('http://127.0.0.1:3000/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Došlo je do greške prilikom slanja zahteva. Status: ' + response.status);
-      }
-    })
-    .then(response => {
-      // Checking server response
-      if (response.success) {
-        // Successfull Login
-        alert("Uspesna prijava")
-        window.open("../index.html");
-        window.location.href = "../index.html";
-      } else {
-        // Neuspesna prijava
-        alert("Failed to login")
-      }
-    })
-    .catch(error => {
-      alert(error.message);
-    });
+document.getElementById('signup').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let data = {
+    username: username,
+    password: password
+  };
+
+  fetch('http://localhost:3000/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Došlo je do greške prilikom slanja zahteva. Status: ' + response.status);
+    }
+  })
+  .then(response => {
+    alert(response.message);
+    window.open("../index.html");
+    window.location.href = "../index.html";
+  })
+  .catch(error => {
+    alert(error.message);
   });
+});
